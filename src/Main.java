8463ebbe6cad075f3cf2ccd524da2532ff8a3321
Jimmy Nguyen <j99717690@gmail.com>
2018-11-27 18:29:00 -0800

    Finished base model for scrambling any amount of words

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.util.Random;
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
+        Main main = new Main();
+        main.scrambled();
+    }
+
+    private void scrambled() {
+        Random r = new Random();
         System.out.println("How many words would you like to scramble?");
         Scanner numberOfPositions = new Scanner(System.in);
         Scanner word = new Scanner(System.in);
@@ -15,5 +21,21 @@ public class Main {
             words[i] = word.next();
         }
 
+        String scrambledWord = null;
+        for (String a : words) {
+            char [] b = a.toCharArray();
+            for (int k = 0; k < b.length; k++) {
+
+                int j = r.nextInt(b.length);
+                char temp = b[k];
+                b[k] = b[j];
+                b[j] = temp;
+                scrambledWord = new String(b).toLowerCase();
+
+            }
+            System.out.println(scrambledWord);
+            System.out.println("______________");
+            System.out.println();
+        }
     }
 }
