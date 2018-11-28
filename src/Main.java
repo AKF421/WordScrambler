6fdd89308c80074eb4528186d619bd0c3ec85a62
Jimmy Nguyen <j99717690@gmail.com>
2018-11-27 17:17:09 -0800

    Created array with the amount of positions given by user and elements given by user.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("How many words would you like to scramble?");
+        Scanner numberOfPositions = new Scanner(System.in);
+        Scanner word = new Scanner(System.in);
+        int arraySize;
+        arraySize = numberOfPositions.nextInt();
 
+        System.out.println("Type the words you would like to scramble.");
+
+        String[] words = new String[arraySize];
+        for (int i = 0; i < words.length; i++) {
+            words[i] = word.next();
+        }
 
     }
 }
