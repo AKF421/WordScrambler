edcf1d85fa3c518d3e86c289f6c7a86207a3c2eb
Jimmy Nguyen <j99717690@gmail.com>
2018-11-30 17:19:23 -0800

    Merged The Train back into development. Can now detect and fix repeats

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,17 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.util.ArrayList;
 public class Main {
+
     public static void main(String[] args) {
-        Main main = new Main();
-        main.scrambled();
-    }
-
-    private void scrambled() {
-        Random r = new Random();
-        System.out.println("How many words would you like to scramble?");
-        Scanner numberOfPositions = new Scanner(System.in);
-        Scanner word = new Scanner(System.in);
-        int arraySize;
-        arraySize = numberOfPositions.nextInt();
-
-        System.out.println("Type the words you would like to scramble.");
-
-        String[] words = new String[arraySize];
-        for (int i = 0; i < words.length; i++) {
-            words[i] = word.next();
-        }
-
-        String scrambledWord = null;
-        for (String a : words) {
-            char [] b = a.toCharArray();
-            for (int k = 0; k < b.length; k++) {
-
-                int j = r.nextInt(b.length);
-                char temp = b[k];
-                b[k] = b[j];
-                b[j] = temp;
-                scrambledWord = new String(b).toLowerCase();
-
-            }
-            System.out.println(scrambledWord);
-            System.out.println("______________");
+        Scrambler scrambler = new Scrambler();
+        ArrayList<String> scrambledWords = new ArrayList<>();
+        scrambler.getScrambledWords(scrambledWords);
+        for(String a : scrambledWords){
+            System.out.println(a);
+            System.out.println("_________");
             System.out.println();
+            System.out.println();
+
         }
+
     }
 }
--- src/Scrambler.java
+++ src/Scrambler.java
@@ -1,6 +1,52 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Scrambler {
+    private Scanner numberOfPositions = new Scanner(System.in);
+    private Scanner wordBeforeScramble = new Scanner(System.in);
 
+    private int arraySize;
+
+    public ArrayList<String> getScrambledWords(ArrayList<String> a) {
+        Random r = new Random();
+        System.out.println("How many words would you like to scramble?");
+        arraySize = numberOfPositions.nextInt();
+        System.out.println("Type the words you would like to scramble.");
+
+        ArrayList<String> wordsBeforeScramble = new ArrayList<>();
+
+/* Adds words given by the user to an array list.
+Those words are scrambled and then put into another array list.
+*/
+        for (int i = 0; i < arraySize; i++) {
+            wordsBeforeScramble.add(i, wordBeforeScramble.next());
+        }
+
+        for (String b : wordsBeforeScramble) {
+            a.add(scramble(b, r));
+        }
+        return a;
+    }
+
+    private String scramble(String word, Random random) {
+        char[] b = word.toCharArray();
+        for(int i = 0; i < b.length; i++){
+            int j = random.nextInt(b.length);
+            char temp = b[0];
+            b[0] = b[j];
+            b[j] = temp;
+        }
+
+        String scrambledWord = new String(b).toLowerCase();
+/*
+If the scrambled word is equal to the original word,
+the method runs again until the scrambled word is different from the original.
+*/
+        if (scrambledWord.toLowerCase().equals(word)) {
+            return scramble(word, random);
+        } else {
+            return scrambledWord;
+        }
+    }
 }
