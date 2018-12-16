6b6c87411872dee9c6218f2fe025021c700c4509
JimmyNguyen <j99717690@gmail.com>
2018-12-16 14:40:55 -0800

    user will be prompted to try again when their input is not valid. method of checking the words user inputs could be improved

--- src/com/worldScrambler/Scrambler.java
+++ src/com/worldScrambler/Scrambler.java
@@ -5,30 +5,45 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 class Scrambler {
+
     private Scanner numberOfPositions = new Scanner(System.in);
     private Scanner wordBeforeScramble = new Scanner(System.in);
     private ArrayList<String> userInput = new ArrayList<>();
 
     public void getScrambledWords(ArrayList<String> scrambledWords) {
+        Integer value = null;
         Random r = new Random();
         System.out.println("How many words would you like to scramble?");
-        int arraySize = numberOfPositions.nextInt();
-        System.out.println("Type the words you would like to scramble.");
+        String arraySize = numberOfPositions.nextLine();
 
-//Adds words given by the user to an array list.
-//Those words are scrambled and then put into another array list.
-
-        for (int i = 0; i < arraySize; i++) {
-            userInput.add(i, wordBeforeScramble.next());
+        try {
+            value = Integer.valueOf(arraySize);
+        } catch (final NumberFormatException e) {
+            System.out.println("Please try again and enter a whole number.");
+            System.exit(0);
         }
+            System.out.println("Type the words you would like to scramble.");
 
-        for (String b : userInput) {
-            scrambledWords.add(scramble(b, r));
+            //Adds words given by the user to an array list.
+            //Those words are scrambled and then put into another array list.
+            for (int i = 0; i < value; i++) {
+                String originalWord = wordBeforeScramble.next();
+                if (!(isNumeric(originalWord))) {
+                    userInput.add(i, originalWord);
+                } else {
+                    System.out.println("Please try again and enter a word");
+                    System.exit(0);
+                }
+            }
+
+            for (String b : userInput) {
+                    scrambledWords.add(scramble(b, r));
+            }
         }
-    }
 
     private String scramble(String word, Random random) {
         char[] b = word.toCharArray();
+
         for (int i = 0; i < b.length; i++) {
             int j = random.nextInt(b.length);
             char temp = b[0];
@@ -46,5 +61,16 @@ class Scrambler {
         } else {
             return scrambledWord;
         }
+    }
+
+    private static boolean isNumeric(String str)
+    {
+        try {
+            double d = Double.parseDouble(str);
+        }
+        catch(NumberFormatException nfe) {
+            return false;
+        }
+        return true;
     }
 }
