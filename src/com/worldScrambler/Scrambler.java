bf155f56180fccfc30926ddf281f0f81a905622d
JimmyNguyen <j99717690@gmail.com>
2018-12-16 23:02:54 -0800

    checks for if user inputs have one letter and improved method for scrambling

--- src/com/worldScrambler/Scrambler.java
+++ src/com/worldScrambler/Scrambler.java
@@ -10,7 +10,7 @@ class Scrambler {
     private Scanner wordBeforeScramble = new Scanner(System.in);
     private ArrayList<String> userInput = new ArrayList<>();
 
-    public void getScrambledWords(ArrayList<String> scrambledWords) {
+    void getScrambledWords(ArrayList<String> scrambledWords) {
         Integer value = null;
         Random r = new Random();
         System.out.println("How many words would you like to scramble?");
@@ -22,32 +22,40 @@ class Scrambler {
             System.out.println("Please try again and enter a whole number.");
             System.exit(0);
         }
-            System.out.println("Type the words you would like to scramble.");
+        System.out.println("Type the words you would like to scramble.");
 
-            //Adds words given by the user to an array list.
-            //Those words are scrambled and then put into another array list.
-            for (int i = 0; i < value; i++) {
-                String originalWord = wordBeforeScramble.next();
-                if (!(isNumeric(originalWord))) {
-                    userInput.add(i, originalWord);
-                } else {
-                    System.out.println("Please try again and enter a word");
-                    System.exit(0);
-                }
-            }
-
-            for (String b : userInput) {
-                    scrambledWords.add(scramble(b, r));
+        //Adds words given by the user to an array list.
+        //Those words are scrambled and then put into another array list.
+        for (int i = 0; i < value; i++) {
+            String originalWord = wordBeforeScramble.next();
+            if (!(isNumeric(originalWord))) {
+                userInput.add(i, originalWord);
+            } else {
+                System.out.println("Please try again and enter a word");
+                System.exit(0);
             }
         }
 
+        for (String b : userInput) {
+            scrambledWords.add(scramble(b, r));
+        }
+    }
+
     private String scramble(String word, Random random) {
+
         char[] b = word.toCharArray();
+
+        try {
+            char secondLetter = b[1];
+        } catch (ArrayIndexOutOfBoundsException a) {
+            System.out.println("Please try again and enter a word with more than one letter -.-");
+            System.exit(0);
+        }
 
         for (int i = 0; i < b.length; i++) {
             int j = random.nextInt(b.length);
-            char temp = b[0];
-            b[0] = b[j];
+            char temp = b[i];
+            b[i] = b[j];
             b[j] = temp;
         }
 
@@ -56,19 +64,17 @@ class Scrambler {
 //If the scrambled word is equal to the original word,
 //the method runs again until the scrambled word is different from the original.
 
-        if (scrambledWord.toLowerCase().equals(word)) {
+        if (scrambledWord.equals(word)) {
             return scramble(word, random);
         } else {
             return scrambledWord;
         }
     }
 
-    private static boolean isNumeric(String str)
-    {
+    private static boolean isNumeric(String str) {
         try {
-            double d = Double.parseDouble(str);
-        }
-        catch(NumberFormatException nfe) {
+            Double.parseDouble(str);
+        } catch (NumberFormatException nfe) {
             return false;
         }
         return true;
